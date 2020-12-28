0e231c44fbf7f8ee4de889f8e50ae635b198a5e4
AllanJeremy <aj.dev254@gmail.com>
2020-12-28 18:35:21 +0300

    [Feature] Created incrementer/decrementer Basics of elm

---
+++ src/Incrementer.elm
@@ -0,0 +1,54 @@
+module Incrementer exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (onClick)
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- Model
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+
+-- Update
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (Debug.toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
